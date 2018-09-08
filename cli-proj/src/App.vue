273f2b54cc4bd5336fc14085d46bc8e21de756d6
Konrad Mikucki <komik966@gmail.com>
2018-09-08 11:21:43 +0200

    [#2.1] [#2.2]

---
+++ cli-proj/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <ul>
+            <li v-for="item in list">
+                {{item.id}} {{ item.name }}
+                <button @click="removeItem(item.id)">x</button>
+            </li>
+        </ul>
+        <input type="text" v-model="newItemName"/>
+        <button @click="addItem()">Add item</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                list: [{id: 1, name: "Foo"}, {id: 2, name: "Bar"}],
+                newItemName: ''
+            }
+        },
+        methods: {
+            addItem() {
+                this.list.push({id: this.list[this.list.length - 1].id + 1, name: this.newItemName})
+            },
+            removeItem(id) {
+                this.list = this.list.filter(item => item.id !== id);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
