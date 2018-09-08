64dbbe362c7acb4b52ffe372d02687edafa3783a
Konrad Mikucki <komik966@gmail.com>
2018-09-08 11:48:15 +0200

    [#2.3] use uuids, add validation

--- cli-proj/src/App.vue
+++ cli-proj/src/App.vue
@@ -1,17 +1,25 @@
 <template>
     <div id="app">
         <ul>
-            <li v-for="item in list">
+            <li v-for="item in list" :key="item.id">
                 {{item.id}} {{ item.name }}
                 <button @click="removeItem(item.id)">x</button>
             </li>
         </ul>
-        <input type="text" v-model="newItemName"/>
-        <button @click="addItem()">Add item</button>
+        <form @submit.prevent="addItem()">
+            <input type="text" v-model="newItemName" name="newItemName" v-validate="'required|min:3'"/>
+            <button>Add item</button>
+
+            <div v-show="errors.has('newItemName')">
+                {{ errors.first('newItemName') }}
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+    import uuid from 'uuid/v4';
+
     export default {
         name: 'app',
         data() {
@@ -22,7 +30,15 @@
         },
         methods: {
             addItem() {
-                this.list.push({id: this.list[this.list.length - 1].id + 1, name: this.newItemName})
+                this.$validator.validateAll().then(result => {
+                    if (!result) {
+                        return;
+                    }
+
+                    this.list.push({id: uuid(), name: this.newItemName})
+
+                    this.$validator.reset();
+                });
             },
             removeItem(id) {
                 this.list = this.list.filter(item => item.id !== id);
